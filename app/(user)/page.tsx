619c333eb971cfb9c0bb44009ccc485ca3daa34e
mariosknl <mariosknl@outlook.com>
2025-05-09 07:52:58 +0300

    Add new components and enhance post functionality
    - Introduced new components: HeroBanner, InformationPanel, PostsList, Post, LockedPost, MemberButton, and FilterByTierSelect for improved user interface and post management.
    - Implemented CreatedAt component for displaying post creation time using react-timeago.
    - Enhanced the Home page to include dynamic content based on user membership tier and recent posts.
    - Updated package.json to include new dependencies: @icons-pack/react-simple-icons and @portabletext/react for better icon support and rich text rendering.
    - Added getPosts function to fetch posts with optional tier filtering from Sanity.

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import HeroBanner from "@/components/HeroBanner";
+import InformationPanel from "@/components/InformationPanel";
+import PostsList from "@/components/post/PostsList";
+import { getPosts } from "@/sanity/lib/post/getPosts";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ tier: string }>;
+}) {
+  const { tier } = await searchParams;
+
+  const posts = await getPosts(tier);
+
   return (
-    <div className="">
-      <h1>Hello World</h1>
+    <div>
+      <HeroBanner />
+
+      <div className="-mt-20">
+        <InformationPanel />
+      </div>
+
+      <hr />
+
+      <PostsList posts={posts} />
     </div>
   );
 }
